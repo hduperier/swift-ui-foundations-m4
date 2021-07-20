da39a3662b1c003a53c3263e1b2d6842372e67d0
hduperier <59696097+hduperier@users.noreply.github.com>
2021-07-20 18:35:30 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Harvey Duperier on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
